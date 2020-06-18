e1ea90f2187aea986f86bf2293800cea5f87da9c
GuilhermeCesar029 <guicesar029@gmail.com>
2020-06-18 15:52:46 -0300

    Projeto Final

--- api/routes/cursos.js
+++ api/routes/cursos.js
@@ -27,11 +27,28 @@ router.post('/', (req, res, next) =>{
 
 //Listando um unico registro
 router.get('/:cursoId', (req, res, next)=>{
-    const id = req.params.produtoId;
+    const id = req.params.cursoId;
     Curso.findById(id)
     .exec()
     .then(doc => {
         res.status(200).json(doc);
+    })
+    .catch(err => {
+        res.status(500).json({error:err});
+    })
+});
+
+//listando todos os cursos
+router.get('/', (req, res, next)=>{
+    const curso = Curso.find({})
+    //const id = req.params.cursoId;
+    //Curso.findById(id)
+    .exec()
+    .then(curso => {
+        res.status(201).json({
+            message: 'Todos os cursos',
+            cursos: curso
+        });
     })
     .catch(err => {
         res.status(500).json({error:err});
@@ -74,6 +91,8 @@ router.put('/:cursoId', (req, res, next) =>{
         });
     });
 });
+
+
 
 
 module.exports = router;
